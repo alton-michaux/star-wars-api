3d2d49b37c29d2a692758d38f87721ff0a0fee8a
Alton Michaux <alton.michaux@yahoo.com>
2022-08-18 00:51:19 -0400

    still working on writing response to html

--- app.js
+++ app.js
@@ -5,17 +5,29 @@ const domElements = {
     submit: document.querySelector('#submit')
 }
 
+function clearHTML() {
+    domElements['columnA'].innerHTML = ''
+    domElements['columnB'].innerHTML = ''
+}
+
 async function fetchResponse() {
+    clearHTML()
     let baseURL = 'https://www.swapi.tech/api/'
     let database = String(document.querySelector('#database').value.toLowerCase())
     let response = await fetch(`${baseURL}${database}`);
     let data = await response.json();
-    console.log(data)
+    console.log(data.results)
+}
+
+function populateContents() {
+    let data = fetchResponse()
+    for (let i = 0; i < data.results.length; i++) {
+        let div = document.createElement('div')
+        div.innerHTML = data.results[i].name
+        domElements['columnA'].appendChild(div)
+    }
 }
 
 domElements['submit'].addEventListener('click', e => {
-    console.log(e.target)
-    fetchResponse()
+    populateContents()
 });
-
-// addEventListener('submit', fetchResponse());
