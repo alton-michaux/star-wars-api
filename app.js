25870678faac21d85d8c08ef419f62e6f01c338f
Alton Michaux <alton.michaux@yahoo.com>
2022-08-18 00:17:23 -0400

    setting up api call and basic event listeners

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
-const message = "Hello";
-console.log(message);
+const domElements = {
+    columnA: document.querySelector('#col-A'),
+    columnB: document.querySelector('#col-B'),
+    columnC: document.querySelector('#col-C'),
+    submit: document.querySelector('#submit')
+}
+
+async function fetchResponse() {
+    let baseURL = 'https://www.swapi.tech/api/'
+    let database = String(document.querySelector('#database').value.toLowerCase())
+    let response = await fetch(`${baseURL}${database}`);
+    let data = await response.json();
+    console.log(data)
+}
+
+domElements['submit'].addEventListener('click', e => {
+    console.log(e.target)
+    fetchResponse()
+});
+
+// addEventListener('submit', fetchResponse());
