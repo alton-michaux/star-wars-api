b4c4d294dcb25d9efd43e7acfc6bcebf24a35620
Alton Michaux <alton.michaux@yahoo.com>
2022-08-18 20:19:02 -0400

    changed baseURL to working API

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
+const baseURL = 'https://swapi.dev/api/'
+
 const domElements = {
     columnA: document.querySelector('#col-A'),
     columnB: document.querySelector('#col-B'),
     columnC: document.querySelector('#col-C'),
+    database: document.querySelector('#database'),
     submit: document.querySelector('#submit')
 }
 
 domElements['submit'].addEventListener('click', e => {
-    fetchResponse()
+    initialResponse()
 });
 
-async function fetchResponse() {
+async function initialResponse() {
     clearHTML()
-    let baseURL = 'https://www.swapi.tech/api/'
-    let database = String(document.querySelector('#database').value.toLowerCase())
+    let database = String(domElements['database'].value.toLowerCase())
     let response = await fetch(`${baseURL}${database}`);
     let data = await response.json();
-    // console.log(data.results[0])
-    handle(data)
+    handle(data, database)
 }
 
 function clearHTML() {
@@ -25,23 +26,34 @@ function clearHTML() {
     
 }
 
-function handle(response) {
-    if (response.message == 'ok') {
-        populateContents(response)
+function handle(response, type) {
+    if (response) {
+        populateContents(response, type)
     } else {
-        console.log('response error')
+        console.log(response)
     }
 }
 
-async function populateContents(data) {
+async function populateContents(data, type) {
     let select = document.createElement('select')
-    select.setAttribute('class', 'class="mb-3 text-center"')
+    select.setAttribute('class', 'mb-3 text-center')
     select.setAttribute('id', 'contents')
     select.setAttribute('name', 'contents')
     domElements['columnA'].appendChild(select)
-    console.log(select)
+    domElements['select'] = document.querySelector('#contents')
     for (let i = 0; i < data.results.length; i++) {
-        html = `<option class="text-center" value${data.results[i].name}>${data.results[i].name}</option>`
+        html = `<option class="text-center option-select" label="${data.results[i].name}" value="${i}">${data.results[i].name}</option>`
         select.insertAdjacentHTML('beforeend', html)
     }
+    addDataListener(type)
+}
+
+function addDataListener(database) {
+    domElements['select'].addEventListener('change', async function() {
+        let index = domElements['select'].value
+        let response = await fetch(`${baseURL}${database}/${index}`);
+        let data = await response.json();
+        console.log(data)
+        // populateData(data)
+    } )
 }
